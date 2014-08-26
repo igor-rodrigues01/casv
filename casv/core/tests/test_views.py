2b6c7905c1806e1ab6de3d1f310e9c6e38fea895
Wille Marcel <wille@wille.blog.br>
2014-08-26 18:06:34 -0300

    add more tests

--- casv/core/tests/test_views.py
+++ casv/core/tests/test_views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from os.path import abspath
 
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
@@ -9,45 +9,61 @@ from ..models import Asv
 from ..views import handle_uploaded_file
 
 
-client = Client()
-client2 = Client()
-
-
 class IndexTest(TestCase):
     def test_index_response(self):
-        response = client.get(reverse('core:index'))
+        response = self.client.get(reverse('core:index'))
         self.assertEqual(response.status_code, 200)
 
 
 class UploadTest(TestCase):
     def setUp(self):
-        password = 'mypassword'
-        user = User.objects.create_superuser('user', 'i@test.com', password)
-        client2.login(username=user.username, password=password)
+        self.user = User.objects.create_superuser('user', 'i@t.com', 'password')
 
-    def test_upload_response(self):
-        response = client.get(reverse('core:upload'))
-        self.assertEqual(response.status_code, 302)  # test without login
-        logged_response = client2.get(reverse('core:upload'))
-        self.assertEqual(logged_response.status_code, 200)  # test with login
+    def test_unlogged_response(self):
+        response = self.client.get(reverse('core:upload'))
+        self.assertRedirects(response, '/login/?next=/upload/')
+
+    def test_logged_response(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('core:upload'))
+        self.assertEqual(response.status_code, 200)
 
 
 class UploadSuccessTest(TestCase):
     def test_success_response(self):
-        response = client.get(reverse('core:upload_success'))
+        response = self.client.get(reverse('core:upload_success'))
         self.assertEqual(response.status_code, 200)
 
 
-class LoginTest(TestCase):
+class LoginLogoutTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser('user', 'i@t.com', 'password')
+
     def test_login_response(self):
-        response = client.get(reverse('core:login'))
+        response = self.client.get(reverse('core:login'))
         self.assertEqual(response.status_code, 200)
+
+    def test_logout_response(self):
+        response = self.client.get(reverse('core:logout'))
+        self.assertRedirects(response, reverse('core:index'))
+
+    def test_loging_logout_user(self):
+        self.client.post(reverse('core:login'),
+            {'username': self.user.username, 'password': 'password'}
+            )
+        self.assertIn('_auth_user_id', self.client.session)
+
+        self.client.get(reverse('core:logout'))
+        self.assertNotIn('_auth_user_id', self.client.session)
 
 
 class HandleUploadedFileTest(TestCase):
     def setUp(self):
-        user = User.objects.create_superuser('user', 'i@test.com', 'mypassword')
-        handle_uploaded_file(abspath('core/fixtures/test-import-shape.zip'), user)
+        self.user = User.objects.create_superuser('user', 'i@t.com', 'password')
+        handle_uploaded_file(
+            abspath('core/fixtures/test-import-shape.zip'),
+            self.user
+            )
 
     def test_import_shape(self):
         self.assertEqual(Asv.objects.all().count(), 1)
